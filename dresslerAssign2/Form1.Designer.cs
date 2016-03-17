7e36e1f49e48bff024deda456b98631470b3e724
Jesse Hallam <jesse.colin.hallam@gmail.com>
2016-03-17 19:52:10 -0400

    Josh's initial work.

---
+++ dresslerAssign2/Form1.Designer.cs
@@ -0,0 +1,188 @@
+﻿namespace dresslerAssign2
+{
+    partial class hangmanForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.hangmanPictureBox = new System.Windows.Forms.PictureBox();
+            this.targetLabel = new System.Windows.Forms.Label();
+            this.targetTB = new System.Windows.Forms.TextBox();
+            this.guessesLabel = new System.Windows.Forms.Label();
+            this.guessesTB = new System.Windows.Forms.TextBox();
+            this.newGameButton = new System.Windows.Forms.Button();
+            this.wonLabel = new System.Windows.Forms.Label();
+            this.wonTB = new System.Windows.Forms.TextBox();
+            this.lossesLabel = new System.Windows.Forms.Label();
+            this.lossesTB = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.hangmanPictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // hangmanPictureBox
+            // 
+            this.hangmanPictureBox.Image = global::dresslerAssign2.Properties.Resources.hangman0;
+            this.hangmanPictureBox.Location = new System.Drawing.Point(69, 32);
+            this.hangmanPictureBox.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.hangmanPictureBox.Name = "hangmanPictureBox";
+            this.hangmanPictureBox.Size = new System.Drawing.Size(356, 315);
+            this.hangmanPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.hangmanPictureBox.TabIndex = 0;
+            this.hangmanPictureBox.TabStop = false;
+            this.hangmanPictureBox.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // targetLabel
+            // 
+            this.targetLabel.AutoSize = true;
+            this.targetLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.targetLabel.Location = new System.Drawing.Point(489, 89);
+            this.targetLabel.Name = "targetLabel";
+            this.targetLabel.Size = new System.Drawing.Size(85, 29);
+            this.targetLabel.TabIndex = 1;
+            this.targetLabel.Text = "Target";
+            // 
+            // targetTB
+            // 
+            this.targetTB.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.targetTB.Location = new System.Drawing.Point(699, 86);
+            this.targetTB.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.targetTB.Name = "targetTB";
+            this.targetTB.ReadOnly = true;
+            this.targetTB.Size = new System.Drawing.Size(277, 36);
+            this.targetTB.TabIndex = 2;
+            this.targetTB.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // guessesLabel
+            // 
+            this.guessesLabel.AutoSize = true;
+            this.guessesLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.guessesLabel.Location = new System.Drawing.Point(489, 199);
+            this.guessesLabel.Name = "guessesLabel";
+            this.guessesLabel.Size = new System.Drawing.Size(113, 29);
+            this.guessesLabel.TabIndex = 3;
+            this.guessesLabel.Text = "Guesses";
+            // 
+            // guessesTB
+            // 
+            this.guessesTB.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.guessesTB.Location = new System.Drawing.Point(699, 196);
+            this.guessesTB.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.guessesTB.Name = "guessesTB";
+            this.guessesTB.Size = new System.Drawing.Size(277, 36);
+            this.guessesTB.TabIndex = 4;
+            this.guessesTB.TextChanged += new System.EventHandler(this.guessesTB_TextChanged);
+            // 
+            // newGameButton
+            // 
+            this.newGameButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.newGameButton.Location = new System.Drawing.Point(197, 394);
+            this.newGameButton.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.newGameButton.Name = "newGameButton";
+            this.newGameButton.Size = new System.Drawing.Size(116, 74);
+            this.newGameButton.TabIndex = 5;
+            this.newGameButton.Text = "New Game";
+            this.newGameButton.UseVisualStyleBackColor = true;
+            this.newGameButton.Click += new System.EventHandler(this.newGameButton_Click);
+            // 
+            // wonLabel
+            // 
+            this.wonLabel.AutoSize = true;
+            this.wonLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.wonLabel.Location = new System.Drawing.Point(489, 406);
+            this.wonLabel.Name = "wonLabel";
+            this.wonLabel.Size = new System.Drawing.Size(72, 29);
+            this.wonLabel.TabIndex = 6;
+            this.wonLabel.Text = "Won:";
+            // 
+            // wonTB
+            // 
+            this.wonTB.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.wonTB.Location = new System.Drawing.Point(579, 402);
+            this.wonTB.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.wonTB.Name = "wonTB";
+            this.wonTB.ReadOnly = true;
+            this.wonTB.Size = new System.Drawing.Size(64, 36);
+            this.wonTB.TabIndex = 7;
+            // 
+            // lossesLabel
+            // 
+            this.lossesLabel.AutoSize = true;
+            this.lossesLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.lossesLabel.Location = new System.Drawing.Point(693, 406);
+            this.lossesLabel.Name = "lossesLabel";
+            this.lossesLabel.Size = new System.Drawing.Size(101, 29);
+            this.lossesLabel.TabIndex = 8;
+            this.lossesLabel.Text = "Losses:";
+            // 
+            // lossesTB
+            // 
+            this.lossesTB.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.lossesTB.Location = new System.Drawing.Point(811, 402);
+            this.lossesTB.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.lossesTB.Name = "lossesTB";
+            this.lossesTB.ReadOnly = true;
+            this.lossesTB.Size = new System.Drawing.Size(64, 36);
+            this.lossesTB.TabIndex = 9;
+            // 
+            // hangmanForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1099, 514);
+            this.Controls.Add(this.lossesTB);
+            this.Controls.Add(this.lossesLabel);
+            this.Controls.Add(this.wonTB);
+            this.Controls.Add(this.wonLabel);
+            this.Controls.Add(this.newGameButton);
+            this.Controls.Add(this.guessesTB);
+            this.Controls.Add(this.guessesLabel);
+            this.Controls.Add(this.targetTB);
+            this.Controls.Add(this.targetLabel);
+            this.Controls.Add(this.hangmanPictureBox);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "hangmanForm";
+            this.Text = "Hangman";
+            this.Load += new System.EventHandler(this.hangmanForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.hangmanPictureBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox hangmanPictureBox;
+        private System.Windows.Forms.Label targetLabel;
+        private System.Windows.Forms.TextBox targetTB;
+        private System.Windows.Forms.Label guessesLabel;
+        private System.Windows.Forms.TextBox guessesTB;
+        private System.Windows.Forms.Button newGameButton;
+        private System.Windows.Forms.Label wonLabel;
+        private System.Windows.Forms.TextBox wonTB;
+        private System.Windows.Forms.Label lossesLabel;
+        private System.Windows.Forms.TextBox lossesTB;
+    }
+}
+
---
+++ dresslerAssign2/Form1.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace dresslerAssign2
+{
+    public partial class hangmanForm : Form
+    {
+        String[] words = { "programming", "network", "website", "linux", "windows", "internet" };
+        string targetWord;
+        int ctr;
+        string[] hyphens = new string[15];
+        int winsCtr, lossesCtr, wrongCtr;
+        Bitmap[] pictures = { dresslerAssign2.Properties.Resources.hangman0,
+                              dresslerAssign2.Properties.Resources.hangman1,
+                              dresslerAssign2.Properties.Resources.hangman2,
+                              dresslerAssign2.Properties.Resources.hangman3,
+                              dresslerAssign2.Properties.Resources.hangman4,
+                              dresslerAssign2.Properties.Resources.hangman5,
+                              dresslerAssign2.Properties.Resources.hangman6};
+
+        
+
+        public hangmanForm()
+        {
+            InitializeComponent();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+          
+        }
+
+        private void newGameButton_Click(object sender, EventArgs e)
+        {
+            //Clearing the screen
+            targetTB.Clear();
+            guessesTB.Clear();
+
+            //Code that generates a number for the array position to grab the value from
+            Random randomNumberGenerator = new Random();
+            Int64 randNum = randomNumberGenerator.Next(words.Length);
+            targetWord = words[randNum];
+
+            
+            
+            //Create hyphens
+            for (ctr = 0; ctr <= targetWord.Length; ctr++)
+            {
+                hyphens[ctr] = "-";
+                targetTB.Text += hyphens[ctr];
+                
+            }
+
+
+
+        }
+
+        private void guessesTB_TextChanged(object sender, EventArgs e)
+        {
+
+            try
+            {
+
+                char guess = guessesTB.Text[guessesTB.Text.Length - 1];
+
+                string txt = guessesTB.Text;
+
+
+                if (targetWord.Contains(guess))
+                {
+                    MessageBox.Show("Found letter in target word");
+                  
+                    
+                }
+                else
+                {
+                    wrongCtr++;
+                    MessageBox.Show("Did not find letter in target word");
+                }
+            }
+            
+            catch (Exception)
+            {
+                MessageBox.Show("No word has been selected");
+            }
+            hangmanPictureBox.Image = pictures[wrongCtr];
+
+            if (wrongCtr == 6)
+            {
+                lossesCtr++;
+                targetTB.Clear();
+                wrongCtr = 0;
+            }
+
+            string lossesCtrInt = Convert.ToString(lossesCtr);
+            lossesTB.Text = lossesCtrInt;
+        }
+
+
+
+        private void hangmanForm_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
